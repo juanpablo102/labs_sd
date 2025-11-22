59c2b0e7f9951a95ea524ecda8f95e9b25047253
juanpablo102 <sampaiodeoliveirajuanpablo@gmail.com>
2025-11-22 20:47:07 -0300

    laboratorio API

---
+++ lab_API/demo/src/main/java/com/example/HelloService.java
@@ -0,0 +1,9 @@
+ package com.example;
+import jakarta.ejb.*;
+ // EJB Stateless que retorna uma mensagem
+ @Stateless
+ public class HelloService {
+    public String sayHello(String name) {
+        return "Olá, " + name + "! Bem-vindo à API EJB.";
+    }
+ }
